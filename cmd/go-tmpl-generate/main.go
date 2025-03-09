a052f029392eae79c96f593b2ba678439abfbccb
chris.sotherden <chris@sothenco.com>
2025-03-08 20:28:24 -0500

    Add --clean flag to delete the output directory before executing the templates

--- cmd/go-tmpl-generate/main.go
+++ cmd/go-tmpl-generate/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/csotherden/go-tmpl-ssg/pkg/generator"
 	"log"
 	"os"
+
+	"github.com/csotherden/go-tmpl-ssg/pkg/generator"
 )
 
 func main() {
@@ -13,10 +14,17 @@ func main() {
 	outputDir := flag.String("output", "", "Path to the output directory")
 	sitemap := flag.Bool("sitemap", false, "Generate sitemap.xml")
 	baseURL := flag.String("base-url", "", "Base URL for sitemap.xml")
+	clean := flag.Bool("clean", false, "Clean the output directory before generating")
 	flag.Parse()
 
 	if *templateDir == "" || *outputDir == "" {
 		log.Fatal("Both --template and --output flags must be provided")
+	}
+
+	if *clean {
+		if err := os.RemoveAll(*outputDir); err != nil {
+			log.Fatalf("Error cleaning output directory: %v", err)
+		}
 	}
 
 	if err := os.MkdirAll(*outputDir, os.ModePerm); err != nil {
